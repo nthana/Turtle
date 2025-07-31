65860c6579c856a30fdb74187cf386ac7c14b8f2
nthana <thana_nita@yahoo.com>
2025-07-31 12:57:22 +0700

    Add Library: ThanaNita.Turtles

--- TurtleTest/Display.cs
+++ TurtleTest/Display.cs
@@ -65,7 +65,7 @@ public partial class Display : Form
 
         startedEvent.Set();
 
-        turtleImage = Image.FromFile("turtle.png");
+        turtleImage = TurtleTest.Properties.Resources.turtle; //Image.FromFile("turtle.png");
 
         Debug.WriteLine(ClientSize);
         Debug.WriteLine(DisplayRectangle);
