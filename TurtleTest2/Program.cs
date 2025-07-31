1081e524b31d2e2a9cfa7324624f26d4c6fde3e8
nthana <thana_nita@yahoo.com>
2025-07-31 17:40:45 +0700

    Short Form Example

--- TurtleTest2/Program.cs
+++ TurtleTest2/Program.cs
@@ -1,19 +1,12 @@
 
 using static ThanaNita.Turtles.One;
 
-namespace TurtleTest2;
-
-internal static class Program
+Speed = 800;
+PenColor = Color.Blue;
+for (int i = 0; i < 20; ++i)
 {
-    static void Main()
-    {
-        Speed = 800;
-        PenColor = Color.Blue;
-        for (int i = 0; i < 20; ++i)
-        {
-            Forward(200);
-            TurnRight(88);
-        }
-        Fill(Color.Yellow);
-    }
+    Forward(200);
+    TurnRight(88);
 }
+Fill(Color.Yellow);
+
